cce49be1b4bf57fccae4144d9a308589fbfbc48f
mkowalew <martin.kowalewski@att.net>
2025-01-12 11:17:02 -0600

    Initial Commit

---
+++ src/app/api/index.js
@@ -0,0 +1,12 @@
+import mock from './mock';
+import './notes/NotesData';
+import './chat/Chatdata';
+import './email/EmailData';
+import './ticket/TicketData';
+import './contacts/ContactsData';
+import './eCommerce/ProductsData';
+import './userprofile/PostData';
+import './userprofile/UsersData';
+import './blog/blogData';
+
+mock.onAny().passThrough();
